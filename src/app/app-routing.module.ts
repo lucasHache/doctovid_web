cea594730128ead2920208a53a0b9dbca774ed33
Hache <lucas.hache70@gmail.com>
2020-10-29 10:19:37 +0100

    Creation des services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AccueilComponent} from './accueil/accueil.component';
+import {PatientComponent} from './patient/patient.component';
+import {PraticienComponent} from './praticien/praticien.component';
+import {ResultatComponent} from './resultat/resultat.component';
+import {InfoUserComponent} from './info-user/info-user.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AccueilComponent},
+  {path: 'patient', component: PatientComponent},
+  {path: 'praticien', component: PraticienComponent},
+  {path: 'resultat', component: ResultatComponent},
+  {path: 'info-user', component: InfoUserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
